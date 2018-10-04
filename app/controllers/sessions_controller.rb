42f04aebde14da43822e0a78a642809799a0a263
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-10-04 23:37:30 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    @user = @user.try(:authenticate, params[:user][:password])
+    @user = current_user.try(:authenticate, params[:user][:password])
     return redirect_to '/users/new' unless @user
     session[:user_id] = @user.id
     redirect_to '/welcome'
   end
 
   def destroy
-    session.delete :user_id
+    reset_session
     redirect_to '/login'
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   before_action :authentication_required
-  
+
   def index
     @user = current_user
   end
